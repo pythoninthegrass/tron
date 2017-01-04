9d17235c848b3b87c1a224e2aff565ecc1eadc18
pythoninthegrass <lstephens@lance-mbp.corp.rhapsody.com>
2017-01-03 16:27:03 -0800

    Trimming and McAfee Uninstall Regedit

--- resources/stage_2_de-bloat/stage_2_de-bloat.bat
+++ resources/stage_2_de-bloat/stage_2_de-bloat.bat
@@ -1,33 +1,30 @@
+@echo off
 REM This is required so we can check the errorlevel inside the FOR loop
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-REM Verbose output check
-if /i %VERBOSE%==yes echo Looking for:
+REM Get working directory
+cd /d %~dp0
 
+:: JOB: Remove crapware programs, phase 1: by specific GUID
 REM Loop through the file...
 for /f %%i in (stage_2_de-bloat\oem\programs_to_target_by_GUID.txt) do (
-    REM  ...and for each line: a. check if it is a comment or SET command and b. perform the removal if not
-    if not %%i==:: (
-    if not %%i==set (
-        if /i %VERBOSE%==yes echo    %%i
-        start /wait msiexec /qn /norestart /x %%i >> "%LOGPATH%\%LOGFILE%" 2>nul
+	REM  ...and for each line: a. check if it is a comment or SET command and b. perform the removal if not
+	if not %%i==:: (
+	if not %%i==set (
+		echo %%i
+		start /wait msiexec /qn /norestart /x %%i 2>nul
 
-        REM Reset UpdateExeVolatile. I guess we could check to see if it's flipped, but eh, not really any point since we're just going to reset it anyway
-        reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Updates" /v UpdateExeVolatile /d 0 /f >nul 2>&1
+		REM Reset UpdateExeVolatile. I guess we could check to see if it's flipped, but eh, not really any point since we're just going to reset it anyway
+		reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Updates" /v UpdateExeVolatile /d 0 /f >nul 2>&1
 
-        REM Check if the uninstaller added entries to PendingFileRenameOperations. If it did, export the contents, nuke the key value, then continue on
-        reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v PendingFileRenameOperations >nul 2>&1
-        if !errorlevel!==0 (
-            echo Offending GUID: %%i >> "%RAW_LOGS%\PendingFileRenameOperations_%COMPUTERNAME%_%CUR_DATE%.txt"
-            reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v PendingFileRenameOperations >> "%RAW_LOGS%\PendingFileRenameOperations_%COMPUTERNAME%_%CUR_DATE%.txt"
-            reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v PendingFileRenameOperations /f >nul 2>&1
-            if /i %VERBOSE%==yes echo  ^^! Detected filenames in PendingFileRenameOperations. Entries exported to "%RAW_LOGS%\PendingFileRenameOperations_%COMPUTERNAME%_%CUR_DATE%.txt" and deleted.
-            echo  ^^! Detected filenames in PendingFileRenameOperations. Entries exported to "%RAW_LOGS%\PendingFileRenameOperations_%COMPUTERNAME%_%CUR_DATE%.txt" and deleted. >> "%LOGPATH%\%LOGFILE%"
-            echo ------------------------------------------------------------------- >> "%RAW_LOGS%\PendingFileRenameOperations_%COMPUTERNAME%_%CUR_DATE%.txt"
-            )
-        )
-    )
-)
+		REM Check if the uninstaller added entries to PendingFileRenameOperations. If it did, export the contents, nuke the key value, then continue on
+		reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v PendingFileRenameOperations >nul 2>&1
+		if !errorlevel!==0 (
+			echo Offending GUID: %%i
+			reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v PendingFileRenameOperations
+			reg delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v PendingFileRenameOperations /f >nul 2>&1
+			)
+		)
+	)
 ENDLOCAL DISABLEDELAYEDEXPANSION
 )
-call functions\log.bat "%CUR_DATE% %TIME%    Done."
